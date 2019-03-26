c9038adc2005ad4fa8dfdf1dad9aa6334015899d
roman <gordonfreeman2332@gmail.com>
2019-03-27 01:09:12 +0300

    fixed adaptive errors and some troubles with feedback section

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,28 +1,52 @@
 import Vue from "vue";
 import { stringify } from "postcss";
-
+import Flickity from 'vue-flickity';
 
 
 new Vue({
     el: "#reviews-item",
     template: "#reviews-components",
+    components: {
+      Flickity
+    },
     props: {
         name: String,
         occ: String,
-        author: Object
+        authorPic: String
       },
     data() {
       return {
-        reviews: {}
-      };
+        flickityOptions: {
+          prevNextButtons: false,
+          pageDots: false,
+          groupCells: 2,
+          
+          reviews: []
+        }
+      }
+    }, 
+    
+    methods: {
+      makeArrWithRequiredImages(data) {
+        return data.map(item => {
+          const requiredPic = require(`../images/content/${item.authorPic}`);
+          item.authorPic = requiredPic;
+  
+          return item;
+        });
+      },
+      next() {
+        this.$refs.flickity.next();
+      },
+      
+      previous() {
+        this.$refs.flickity.previous();
+      }
     },
     created() {
       const data = require("../data/reviews.json");
       
       console.log(JSON.stringify(data));
-      this.reviews = data;
-
-
-      //console.log(data[1].author-name);
+      this.reviews = this.makeArrWithRequiredImages(data);
     }
   });
