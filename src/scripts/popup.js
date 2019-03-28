1410bba172eea52a1daf01798440c141a5e91813
roman <gordonfreeman2332@gmail.com>
2019-03-28 03:59:26 +0300

    popup menu btn and adaptive for tablets and phones

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,5 +1,49 @@
-const popup = document.querySelector('.burger-menu');
+const humMenu = document.querySelector('.burger-menu');
+const main = document.querySelector('.main');
+const nav = document.querySelector('.nav');
+const socials = document.querySelector('.socials');
+const popup = document.createElement('div');
+const closedBtn = document.createElement('button');
+const hedaer = document.querySelector('.header');
+closedBtn.className = 'burgers-menu-closed';
+popup.className = 'popup-menu';
 
-popup.addEventListener('click', e =>{
-    console.log('it works');
+
+
+humMenu.addEventListener('click', e => {
+    const reqHeight = main.children[1].getBoundingClientRect().height;
+    popup.style.height = reqHeight+'px';
+    let op = 0.1;
+    main.appendChild(popup);
+    setTimeout (function foo(){
+        if (op < 1) {
+            op += 0.1;
+            popup.style.opacity= op;
+            setTimeout (foo, 50);
+        }
+    }, 50);
+    setTimeout (function (){
+        nav.classList.add('popup');
+        socials.classList.add('popup');
+        popup.appendChild(closedBtn);
+        popup.appendChild(nav);
+        popup.appendChild(socials);
+    }, 500);
+})
+
+closedBtn.addEventListener('click', e => {
+    let op = 1;
+        setTimeout (function doo(){
+            if (op > 0) {
+                op -= 0.1;
+                popup.style.opacity= op;
+                setTimeout (doo, 50);
+            }
+        },50)
+    setTimeout (function (){
+        hedaer.appendChild(socials);
+        main.removeChild(popup);
+        socials.classList.remove('popup');
+        nav.classList.remove('popup');
+    }, 1000);
 })
